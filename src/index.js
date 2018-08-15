4d84b54cf1b90f223d8227b29577eb089f48de4a
Rohit Wanchoo <rohitwanchoo94@gmail.com>
2018-08-16 03:10:30 +0530

    Mocking in progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import {getUsers} from './api/userApi';
 import './index.css';
 
-import numeral from 'numeral';
+// Populate table of users via API call
+getUsers().then(result => {
+  let usersBody = "";
 
-const courseValue = numeral(1000).format('$0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`;
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
