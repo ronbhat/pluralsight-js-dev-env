7fd483e43721b3f4754177e76e61fd774d87aa2a
Rohit Wanchoo <rohitwanchoo94@gmail.com>
2018-08-17 14:22:23 +0530

    Course completed

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi')? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi')? 'http://localhost:3001/' : 'https://obscure-hamlet-28468.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
